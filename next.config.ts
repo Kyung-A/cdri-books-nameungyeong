1a7be5bfbf0189e40b35973225336ef71aecb61e
Kyung-A <nek1717@naver.com>
2025-05-29 14:21:05 +0900

    feat: add images domains

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["search1.kakaocdn.net"],
+  },
 };
 
 export default nextConfig;
